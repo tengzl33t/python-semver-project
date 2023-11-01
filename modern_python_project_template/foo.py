82f469e7faca861dd0408cedbd44abb5804d7c32
Nick Ferguson <nick.ferguson@threatx.com>
2023-10-31 20:39:12 -0400

    feat: remove bar

--- modern_python_project_template/foo.py
+++ modern_python_project_template/foo.py
@@ -12,20 +12,6 @@ def baz() -> str:
     return "baz"
 
 
-def bar() -> str:
-    """Summary line.
-
-    Extended description of function.
-
-    Args:
-        foo (str): Description of arg1
-
-    Returns:
-        str: Description of return value
-    """
-    return "bar"
-
-
 def foo() -> str:
     """Summary line.
 
